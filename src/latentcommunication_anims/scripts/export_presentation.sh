a34f313cccf9f01aed479052cf30c38c7035e51a
Luca Moschella <luca.moschella94@gmail.com>
2024-03-05 18:00:51 +0100

    Update revealjs export config

--- src/latentcommunication_anims/scripts/export_presentation.sh
+++ src/latentcommunication_anims/scripts/export_presentation.sh
@@ -2,6 +2,14 @@
 
 
 manim-slides convert \
+    -ccontrols=false \
+    -cprogress=true \
+    -cshow_slide_number=false \
+    -coverview=true \
+    -ctouch=true \
+    -cview_distance=6 \
+    -cmobile_view_distance=4  \
+    -chide_cursor_time=1000 \
     TitleAnimation \
     NeuralNetworks \
     ShapeLatentSpace \
