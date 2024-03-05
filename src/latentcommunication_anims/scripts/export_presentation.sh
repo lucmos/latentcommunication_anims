17d22008af640a0671d41216b799bf8caaf396c2
Luca Moschella <luca.moschella94@gmail.com>
2024-03-05 03:30:27 +0100

    Add convert to html script

--- src/latentcommunication_anims/scripts/export_presentation.sh
+++ src/latentcommunication_anims/scripts/export_presentation.sh
@@ -1,1 +1,40 @@
 #!/usr/bin/env bash
+
+
+manim-slides convert \
+    TitleAnimation \
+    NeuralNetworks \
+    ShapeLatentSpace \
+    ParallelOpt \
+    Unifying \
+    RelReps \
+    AnglePreserving \
+    RelRepsAlgo \
+    SemanticSpace \
+    LatentMetric \
+    RelRepStitching \
+    RelRepStitchingVision \
+    RelRepStitchingCrossLingual \
+    Translation \
+    TranslationAlgorithm \
+    AffineTransformation \
+    TransaltionMainDifference \
+    TranslationStitching \
+    TranslationCrossmodal \
+    TranslationOtherResults \
+    Intuition \
+    Correspondence \
+    Limitations \
+    BridgeProblem \
+    BridgeFramework \
+    BridgeResults \
+    BootstrappingIntro \
+    BootstrappingResults \
+    Applying \
+    Asif \
+    RL \
+    Conclusions \
+    FutureWorks \
+    Unireps \
+    Thanks \
+    index.html
