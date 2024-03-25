ff4dd8a96465025680f30fdcabd8f4859b235ab7
Luca Moschella <luca.moschella94@gmail.com>
2024-03-25 16:09:35 +0000

    Update build presentation script

--- src/latentcommunication_anims/scripts/build_presentation.sh
+++ src/latentcommunication_anims/scripts/build_presentation.sh
@@ -1,71 +1,57 @@
 #!/usr/bin/env bash
 
 
-# Intro
-manim src/latentcommunication_anims/slides/title.py TitleAnimation
-manim src/latentcommunication_anims/slides/neural_networks.py NeuralNetworks
-manim src/latentcommunication_anims/slides/shape_latent_space.py ShapeLatentSpace
-manim src/latentcommunication_anims/slides/parallel_opt.py ParallelOpt
-manim src/latentcommunication_anims/slides/unifying.py Unifying
-
-# Relative
-manim src/latentcommunication_anims/slides/relative/relreps.py RelReps
-manim src/latentcommunication_anims/slides/relative/angle_preserving.py AnglePreserving
-manim src/latentcommunication_anims/slides/relative/relreps_algo.py RelRepsAlgo
-manim src/latentcommunication_anims/slides/relative/semantic_space.py SemanticSpace
-manim src/latentcommunication_anims/slides/relative/latent_metric.py LatentMetric
-manim src/latentcommunication_anims/slides/relative/relreps_stitching.py RelRepStitching
-manim src/latentcommunication_anims/slides/relative/relreps_stitching_vision.py RelRepStitchingVision
-manim src/latentcommunication_anims/slides/relative/relreps_stitching_crosslingual.py RelRepStitchingCrossLingual
-
-# Translation
-manim src/latentcommunication_anims/slides/translation/translation.py Translation
-manim src/latentcommunication_anims/slides/translation/translationalgorithm.py TranslationAlgorithm
-manim src/latentcommunication_anims/slides/translation/affinetransformation.py AffineTransformation
-manim src/latentcommunication_anims/slides/translation/maindifference.py TransaltionMainDifference
-manim src/latentcommunication_anims/slides/translation/translation_stitching.py TranslationStitching
-manim src/latentcommunication_anims/slides/translation/crossmodal.py TranslationCrossmodal
-manim src/latentcommunication_anims/slides/translation/results.py TranslationOtherResults
-
-
-# Intuition
-manim src/latentcommunication_anims/slides/formalization.py Formalization
-manim src/latentcommunication_anims/slides/intuition.py Intuition
-manim src/latentcommunication_anims/slides/correspondence.py Correspondence
-
-# Limitations
-manim src/latentcommunication_anims/slides/limitations.py Limitations
-manim src/latentcommunication_anims/slides/bridge/bridge_problem.py BridgeProblem
-manim src/latentcommunication_anims/slides/bridge/bridge_framework.py BridgeFramework
-manim src/latentcommunication_anims/slides/bridge/results.py BridgeResults
-manim src/latentcommunication_anims/slides/bootstrap/bootstrapping_intro.py BootstrappingIntro
-manim src/latentcommunication_anims/slides/bootstrap/bootstrapping_results.py BootstrappingResults
-
-# Applications
-manim src/latentcommunication_anims/slides/applying.py Applying
-manim src/latentcommunication_anims/slides/applications/asif.py Asif
-manim src/latentcommunication_anims/slides/applications/rl.py RL
-
-# Conclusions
-manim src/latentcommunication_anims/slides/conclusions.py Conclusions
-manim src/latentcommunication_anims/slides/futureworks.py FutureWorks
-manim src/latentcommunication_anims/slides/unireps.py Unireps
-manim src/latentcommunication_anims/slides/thanks.py Thanks
+manim src/latentcommunication_anims/slides/title.py TitleAnimation && \
+manim src/latentcommunication_anims/slides/neural_networks.py NeuralNetworks && \
+manim src/latentcommunication_anims/slides/shape_latent_space.py ShapeLatentSpace && \
+manim src/latentcommunication_anims/slides/parallel_opt.py ParallelOpt && \
+manim src/latentcommunication_anims/slides/unifying.py Unifying && \
+manim src/latentcommunication_anims/slides/relative/relreps.py RelReps && \
+manim src/latentcommunication_anims/slides/relative/angle_preserving.py AnglePreserving && \
+manim src/latentcommunication_anims/slides/relative/relreps_algo.py RelRepsAlgo && \
+manim src/latentcommunication_anims/slides/relative/semantic_space.py SemanticSpace && \
+manim src/latentcommunication_anims/slides/relative/latent_metric.py LatentMetric && \
+manim src/latentcommunication_anims/slides/relative/relreps_stitching.py RelRepStitching && \
+manim src/latentcommunication_anims/slides/relative/relreps_stitching_vision.py RelRepStitchingVision && \
+manim src/latentcommunication_anims/slides/relative/relreps_stitching_crosslingual.py RelRepStitchingCrossLingual && \
+manim src/latentcommunication_anims/slides/translation/translation.py Translation && \
+manim src/latentcommunication_anims/slides/translation/translationalgorithm.py TranslationAlgorithm && \
+manim src/latentcommunication_anims/slides/translation/affinetransformation.py AffineTransformation && \
+manim src/latentcommunication_anims/slides/translation/maindifference.py TransaltionMainDifference && \
+manim src/latentcommunication_anims/slides/translation/translation_stitching.py TranslationStitching && \
+manim src/latentcommunication_anims/slides/translation/crossmodal.py TranslationCrossmodal && \
+manim src/latentcommunication_anims/slides/translation/results.py TranslationOtherResults && \
+manim src/latentcommunication_anims/slides/formalization.py Formalization && \
+manim src/latentcommunication_anims/slides/intuition.py Intuition && \
+manim src/latentcommunication_anims/slides/correspondence.py Correspondence && \
+manim src/latentcommunication_anims/slides/limitations.py Limitations && \
+manim src/latentcommunication_anims/slides/bridge/bridge_problem.py BridgeProblem && \
+manim src/latentcommunication_anims/slides/bridge/bridge_framework.py BridgeFramework && \
+manim src/latentcommunication_anims/slides/bridge/results.py BridgeResults && \
+manim src/latentcommunication_anims/slides/bootstrap/bootstrapping_intro.py BootstrappingIntro && \
+manim src/latentcommunication_anims/slides/bootstrap/bootstrapping_results.py BootstrappingResults && \
+manim src/latentcommunication_anims/slides/applying.py Applying && \
+manim src/latentcommunication_anims/slides/applications/asif.py Asif && \
+manim src/latentcommunication_anims/slides/applications/rl.py RL && \
+manim src/latentcommunication_anims/slides/conclusions.py Conclusions && \
+manim src/latentcommunication_anims/slides/futureworks.py FutureWorks && \
+manim src/latentcommunication_anims/slides/unireps.py Unireps && \
+manim src/latentcommunication_anims/slides/thanks.py Thanks 
 
 
-# manim src/latentcommunication_anims/slides/toy_model.py ToyModel
-# manim src/latentcommunication_anims/slides/parallel_opt.py ParallelOpt
-# manim src/latentcommunication_anims/slides/isometric_latent_spaces.py IsometricLatentSpaces
-# manim src/latentcommunication_anims/slides/relreps.py RelReps
-# manim src/latentcommunication_anims/slides/word_embeddings.py SemanticSpace
-# manim src/latentcommunication_anims/slides/latent_metric.py LatentMetric
-# manim src/latentcommunication_anims/slides/stitching.py Stitching
-# manim src/latentcommunication_anims/slides/table4.py CrossLingualStitching
-# manim src/latentcommunication_anims/slides/table6.py VisionStitching
-# manim src/latentcommunication_anims/slides/parallel_anchors.py ParallelAnchors
-# manim src/latentcommunication_anims/slides/parallel_anchors_v2.py ParallelAnchors
-# manim src/latentcommunication_anims/slides/conclusions.py Conclusions
-# manim src/latentcommunication_anims/slides/thanks.py Thanks
-# manim src/latentcommunication_anims/slides/shape_latent_space_sec.py LatentSpaceShape
-# manim src/latentcommunication_anims/slides/parallel_anchors_short.py ParallelAnchorsShort
-# manim src/latentcommunication_anims/slides/translation.py Translation
+# manim src/latentcommunication_anims/slides/toy_model.py ToyModel && \
+# manim src/latentcommunication_anims/slides/parallel_opt.py ParallelOpt && \
+# manim src/latentcommunication_anims/slides/isometric_latent_spaces.py IsometricLatentSpaces && \
+# manim src/latentcommunication_anims/slides/relreps.py RelReps && \
+# manim src/latentcommunication_anims/slides/word_embeddings.py SemanticSpace && \
+# manim src/latentcommunication_anims/slides/latent_metric.py LatentMetric && \
+# manim src/latentcommunication_anims/slides/stitching.py Stitching && \
+# manim src/latentcommunication_anims/slides/table4.py CrossLingualStitching && \
+# manim src/latentcommunication_anims/slides/table6.py VisionStitching && \
+# manim src/latentcommunication_anims/slides/parallel_anchors.py ParallelAnchors && \
+# manim src/latentcommunication_anims/slides/parallel_anchors_v2.py ParallelAnchors && \
+# manim src/latentcommunication_anims/slides/conclusions.py Conclusions && \
+# manim src/latentcommunication_anims/slides/thanks.py Thanks && \
+# manim src/latentcommunication_anims/slides/shape_latent_space_sec.py LatentSpaceShape && \
+# manim src/latentcommunication_anims/slides/parallel_anchors_short.py ParallelAnchorsShort && \
+# manim src/latentcommunication_anims/slides/translation.py Translation && \
