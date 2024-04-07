8a088eb377ef750b1fb3ba1e9a21add548da7a33
Luca Moschella <luca.moschella94@gmail.com>
2024-04-07 16:49:50 +0200

    Add data experiment step 5

---
+++ src/latentcommunication_anims/scripts/start_presentation_datacuration.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+manim-slides present \
+  DataExperimentIntro \
+  DataExperiment1 \
+  DataExperiment2 \
+  DataExperiment3 \
+  DataExperiment4 \
+  DataExperiment5 \
+  DataExperiment6 
